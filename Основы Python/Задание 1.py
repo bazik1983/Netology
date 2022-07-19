cee27c51af336e56f73b4dad64d32eb7808070d3
bazik1983 <bazik83@gmail.com>
2022-07-19 21:41:17 +0300

    initial commit

---
+++ Основы Python/Задание 1.py
@@ -0,0 +1,8 @@
+phrase_1 = input()
+phrase_2 = input()
+if phrase_1 > phrase_2:
+    print('Фраза 1 длиннее фразы 2')
+elif phrase_2 > phrase_1:
+    print('Фраза 2 длиннее фразы 1')
+else:
+    print('Фразы равной длины')
