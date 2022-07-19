9b1baf69d7a60d6255ae57d8a8b1cc825f122000
bazik1983 <bazik83@gmail.com>
2022-07-19 23:32:41 +0300

    initial commit

---
+++ Основы Python/Задание 3.py
@@ -0,0 +1,27 @@
+Month = str(input("Месяц рождения:"))
+Day = str(input("Дата рождения:"))
+if ((Month == 'Декабрь') and (Day >= '23')) or ((Month == 'Январь') and (Day <= '20')):
+    zodiac = "Козерог"
+elif (Month == 'Январь' and Day >= '21') or (Month == 'Февраль' and Day <= '19'):
+    zodiac = "Водолей"
+elif (Month == 'Февраль' and Day >= '20') or (Month == 'Март' and Day <= '20'):
+    zodiac = "Рыбы"
+elif (Month == 'Март' and Day >= '21') or (Month == 'Апрель' and Day <= '20'):
+    zodiac = "Овен"
+elif (Month == 'Апрель' and Day >= '21') or (Month == 'Май' and Day <= '21'):
+    zodiac = "Телец"
+elif (Month == 'Май' and Day >= '22') or (Month == 'Июнь' and Day <= '21'):
+    zodiac = "Близнецы"
+elif (Month == 'Июнь' and Day >= '22') or (Month == 'Июль' and Day <= '22'):
+    zodiac = "Рак"
+elif (Month == 'Июль' and Day >= '23') or (Month == 'Август' and Day <= '21'):
+    zodiac = "Лев"
+elif (Month == 'Август' and Day >= '22') or (Month == 'Сентябрь' and Day <= '23'):
+    zodiac = "Дева"
+elif (Month == 'Сентябрь' and Day >= '24') or (Month == 'Октябрь' and Day <= '23'):
+    zodiac = "Весы"
+elif (Month == 'Октябрь' and Day >= '24') or (Month == 'Ноябрь' and Day <= '23'):
+    zodiac = "Скорпион"
+elif (Month == 'Ноябрь' and Day >= '23') or (Month == 'Декабрь' and Day <= '22'):
+    zodiac = "Стрелец"
+print(zodiac)
