bac995b83b5b49a4dbca887288ad973f8c03b3c7
bazik1983 <bazik83@gmail.com>
2022-08-05 23:45:23 +0300

    commit

--- Функции/Задание1и2.py
+++ Функции/Задание1и2.py
@@ -39,10 +39,10 @@ def list_doc():
     """
     Функция возвращает список документов на полках
     """
-    for i in documents:
-        for item, values in directories.items():
-            if i['number'] in values:
-                print(f"№ {i['number']}, тип: {i['type']}, владелец: {i['name']}, полка хранения: {item}")
+    for shelf in directories:
+        for num in directories[shelf]:
+            found = next(e for e in documents if e['number'] == num)
+            print(f'№: {num}, тип:{found["type"]}, владелец:{found["name"]}, полка хранения: {shelf}')
 
 
 def add_shelf():
@@ -61,17 +61,71 @@ def add_shelf():
 
 def del_shelf():
     """
-    Функция удаляе полку, если она не пустая
+    Функция удаляет полку, если она не пустая
     """
     shelf = input("Введите номер полки: ")
     if shelf in directories:
         if directories[shelf]:
-            return print(f"На полке есть документы, удалите их перед удалением полки. Текущий перечень полок:' {list(directories.keys())}")
+            return print(f"На полке есть документы, удалите их перед удалением полки. Текущий перечень полок: {list(directories.keys())}")
         del directories[shelf]
-        return print(f"Полка удалена. Текущий перечень полок:' {list(directories.keys())}")
-    return print(f"Такой полки не существует. Текущий перечень полок:' {list(directories.keys())}")
+        return print(f"Полка удалена. Текущий перечень полок: {list(directories.keys())}")
+    return print(f"Такой полки не существует. Текущий перечень полок: {list(directories.keys())}")
 
 
+def add_doc():
+    """
+    Функция добавляет документ
+    """
+    doc_type = input("Введите тип документа: ")
+    doc_number = input("Введите номер документа: ")
+    doc_owner = input("Введите имя владельца документа: ")
+    shelf = input("Введите номер полки: ")
+    if shelf not in directories:
+        print(f"Такой полки не существует. Добавьте полку командой ads.\nТекущий список документов:")
+        return list_doc()
+    else:
+        new_doc = dict(type = doc_type, number = doc_number, name = doc_owner)
+        documents.append(new_doc)
+        directories[shelf] += [doc_number]
+        print(f"Документ добавлен. Текущий список документов:")
+        return list_doc()
+
+def del_doc():
+    doc_number = input("Введите номер документа, который хотите удалить: ")
+    initial_len = len(documents)
+    for i, d in enumerate(documents):
+        if d["number"] == doc_number:
+            documents.pop(i)
+    if initial_len == len(documents):
+        print(f"Документ не найден в базе. Добавьте полку командой ads.\nТекущий список документов:")
+        return list_doc()
+    for key, value in directories.items():
+        if doc_number in value:
+            value.remove(doc_number)
+        print(f"Документ удален. Добавьте полку командой ads.\nТекущий список документов:")
+        return list_doc()
+
+
+def move_doc():
+    doc_number = input("Введите номер документа,который хоитите переместить: ")
+    shelf = input("Введит номер полки, на которую хоиите перемести документ: ")
+    doc_existence = False
+
+    if shelf not in directories:
+        return print(f"Полки не существует. Текущий перечень полок: {list(directories.keys())}")
+
+    for key, value in directories.items():
+        if doc_number in value:
+            doc_existence = True
+            directories[shelf] += [doc_number]
+            value.remove(doc_number)
+
+    if doc_existence == True:
+        print(f"Документ перемещен.\nТекущий список документов:")
+        return list_doc()
+    else:
+        print(f"Документ не найден в базе.\nТекущий список документов:")
+        return list_doc()
 
 def main():
     """
@@ -86,11 +140,16 @@ def main():
             shelf()
         elif command_input == 'l':
             list_doc()
-        elif command_input == 'as':
-            number_input = input('Введите номер полки: \n')
+        elif command_input == 'ads':
             add_shelf()
         elif command_input == 'ds':
             del_shelf()
+        elif command_input == 'ad':
+            add_doc()
+        elif command_input == 'd':
+            del_doc()
+        elif command_input == 'm':
+            move_doc()
         elif command_input == 'q':
             break
         else:
